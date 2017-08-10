5e087297a8872e91eaf6086405eea25ae52c691d
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-08-10 12:30:10 -0300

    Adding methods to monthly income and your tests

--- DreamRich/lib/patrimony/patrimony.py
+++ DreamRich/lib/patrimony/patrimony.py
@@ -15,3 +15,18 @@ class Patrimony:
 
   def current_investment_immobilized(assets):
     return Patrimony.sum_elements(assets)
+
+  def monthly_income(sources_of_income):
+    return Patrimony.sum_elements(sources_of_income)
+
+  def monthly_income_bonus(sources_of_income, vacation_bonus, thirteenth_salary):
+    MONTHS = 12
+    total = Patrimony.sum_elements(sources_of_income)
+
+    if(vacation_bonus != 0):
+      total += vacation_bonus / MONTHS
+    
+    if(thirteenth_salary != 0):
+      total += thirteenth_salary / MONTHS
+
+    return total
--- DreamRich/lib/patrimony/tests.py
+++ DreamRich/lib/patrimony/tests.py
@@ -5,8 +5,13 @@ class PatrimonyTest(TestCase):
 
   assets = [1000, 2000, 3000, 4000]
   debts = [500, 700]
+  sources_of_income = [3000, 1000, 1500, 7500]
+  vacation_bonus = 6000
+  thirteenth_salary = 7000
   current_investment_net = 8800
   current_investment_immobilized = 10000
+  monthly_income = 13000
+  monthly_income_bonus = 14083.33
 
   def test_current_investment_net(self):
     current_net = Patrimony.current_investment_net(self.assets, self.debts)
@@ -15,3 +20,11 @@ class PatrimonyTest(TestCase):
   def test_current_investment_immobilized(self):
     current_immobilized = Patrimony.current_investment_immobilized(self.assets)
     self.assertEqual(current_immobilized, self.current_investment_immobilized)
+
+  def test_monthly_income(self):
+    income = Patrimony.monthly_income(self.sources_of_income)
+    self.assertEqual(income, self.monthly_income)
+
+  def test_monthly_income_bonus(self):
+    income = Patrimony.monthly_income_bonus(self.sources_of_income, self.vacation_bonus, self.thirteenth_salary)
+    self.assertAlmostEqual(income, self.monthly_income_bonus, 2)
