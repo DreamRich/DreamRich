4ae5e77989a345fe56e23ed078a00ddd10736271
Marcelo Ferreira <marcelohpf@gmail.com>
2017-08-11 15:26:35 -0300

    Autoload factories and rename package, removing camel case
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

---
+++ DreamRich/dreamrich/management/commands/seed.py
@@ -0,0 +1,30 @@
+import inspect
+from django.core.management.base import BaseCommand
+from dreamrich.settings import LOCAL_APPS
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        for app in LOCAL_APPS:
+            try:
+                self.stdout.write("Load app({}) module".format(app))
+                app_module = __import__("{}.factory".format(app)).factory
+                self.stdout.write("{} has factories :) ".format(app))
+                for name, obj in inspect.getmembers(app_module):
+                    if inspect.isclass(obj) and name.find('Factory') != -1:
+                        self.stdout.write("Creating objects to {}".format(app))
+                        for n in range(kwargs['loop']):
+                            getattr(obj, 'create')()
+                            self.stdout.write(".", ending="")
+            except AttributeError:
+                self.stderr.write("{} don't have factories :( ".format(app))
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-n',
+            type=int,
+            dest='loop',
+            default=3,
+            help='The number of each class to be created'
+        )
