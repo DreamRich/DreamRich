da7281d3992c063df3ec6f19ad701e4ee4a99370
Icaro Pires <icaropiresdf@gmail.com>
2017-08-23 21:49:29 -0300

    Add missing configuration for permissions on settings
    Signed-off-by: Icaro Pires <icaropiresdf@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- DreamRich/dreamrich/settings.py
+++ DreamRich/dreamrich/settings.py
@@ -101,9 +101,13 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+    #    'rest_framework.permissions.IsAuthenticated',
+        "rest_framework.permissions.AllowAny",
     )
 }
 
