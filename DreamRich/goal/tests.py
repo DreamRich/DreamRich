242bdef179a724301a7ab93e3930fa646abcb250
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-09-21 11:35:21 -0300

    Testing goals_flow_dic

--- DreamRich/goal/tests.py
+++ DreamRich/goal/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from decimal import Decimal
 from financial_planning.factories import FinancialPlanningFactory
 from client.factories import ActiveClientMainFactory
-from goal.factories import GoalFactory, GoalManagerFactory
+from goal.factories import GoalFactory, GoalManagerFactory, GoalTypeFactory
 import datetime
 
 
@@ -14,12 +14,16 @@ class GoalTest(TestCase):
         financial_planning = FinancialPlanningFactory(active_client=\
                                                            active_client)
         self.goal_manager = financial_planning.goal_manager
+        goal_type1 = GoalTypeFactory(name='Imóvel')
+        goal_type2 = GoalTypeFactory(name='Vestuário')
         self.goal_has_end_date = GoalFactory(has_end_date = True,
+                                         goal_type = goal_type1,
                                          year_init = 2021,
                                          periodicity = 3,
                                          value = 50000,
                                          goal_manager = self.goal_manager)
         self.goal_hasnt_end_date = GoalFactory(has_end_date = False,
+                                         goal_type = goal_type2,
                                          year_init = 2021,
                                          periodicity = 3,
                                          value = 50000,
@@ -42,3 +46,15 @@ class GoalTest(TestCase):
     def test_flow_has_end_date(self):
         self.assertEqual(self.array_flow_with_date,
                          self.goal_hasnt_end_date.flow)
+    def test_goals_flow_dic(self):
+        goals_flow_dic = []
+        goal_flow_dic1 = {}
+        goal_flow_dic1['name'] = 'Imóvel'
+        goal_flow_dic1['data'] = self.array_flow_withot_date
+        goal_flow_dic2 = {}
+        goal_flow_dic2['name'] = 'Vestuário'
+        goal_flow_dic2['data'] = self.array_flow_with_date
+        goals_flow_dic.append(goal_flow_dic1)
+        goals_flow_dic.append(goal_flow_dic2)
+
+        self.assertEqual(goals_flow_dic,self.goal_manager.goals_flow_dic)
