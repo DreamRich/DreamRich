5f773f1be52ef7569eac081cb951bdce763eadbd
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-09-27 12:41:27 -0300

    Fixing pylint of seed goal type

--- DreamRich/dreamrich/management/commands/seed_goal_type.py
+++ DreamRich/dreamrich/management/commands/seed_goal_type.py
@@ -1,3 +1,4 @@
+# pylint: disable=unused-argument
 import yaml
 from django.core.management.base import BaseCommand
 from goal.models import GoalType
@@ -7,7 +8,7 @@ FILE_PATH = 'dreamrich/management/commands/data/goal_type.yaml'
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        with open(FILE_PATH, 'r') as f:
-            seed_data = yaml.load(f)
+        with open(FILE_PATH, 'r') as element:
+            seed_data = yaml.load(element)
             for registry in seed_data:
                 GoalType.objects.create(**registry)
