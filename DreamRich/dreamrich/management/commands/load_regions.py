5ca5fd71a0f10b90fbe28de77ba9b341b7541365
Marcelo Ferreira <marcelohpf@gmail.com>
2017-10-03 14:30:08 -0300

    Finish the parser from yaml
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- DreamRich/dreamrich/management/commands/load_regions.py
+++ DreamRich/dreamrich/management/commands/load_regions.py
@@ -5,59 +5,34 @@ from django.core.management.base import BaseCommand
 from dreamrich.settings import BASE_DIR
 from client.models import Country, State
 
-FILE_PATH = 'dreamrich/management/commands/data/regions_data_list'
 
 class Command(BaseCommand):
-    @staticmethod
-    def _parse_line(line):
-        # Split on last space and rotate back
-        name, abbreviation = line[::-1].split(maxsplit=1)
-        name, abbreviation = abbreviation[::-1], name[::-1]
-
-        return name, abbreviation
 
     def handle(self, *args, **kwargs):
         file_path = os.path.join(BASE_DIR, 'dreamrich', 'seed',
             'regions_data_list.yml')
+
+        self.stdout.write('Load seed from {}'.format(file_path))
         with open(file_path) as load_file:
-            # yaml.load(load_file)
-            for line in load_file:
-                line = line.lstrip()
+            try:
+                data = yaml.load(load_file)
+                for country in data:
+                    states = country.pop('states', [])
 
-                if not line:
-                    continue
+                    new_country, created = Country.objects.get_or_create(
+                            **country)
+                    self.stdout.write('\tCountry {name} - {abbreviation} '
+                            'was registered'.format(**country))
 
-                elif line[0:2] == "->":
-                    line = line[3:] # 3 to take space as well
+                    for state in states:
+                        State.objects.get_or_create(country=new_country,
+                                **state)
+                        self.stdout.write('\t\tState {name} - {abbreviation} '
+                            'was registered'.format(**state))
 
-                    try:
-                        country, abbreviation = self._parse_line(line)
-                    except ValueError:
-                        self.stderr.write('Probably the file format is wrong')
-
-                    Country.objects.create(
-                        name=country,
-                        abbreviation=abbreviation
-                    )
-                    self.stdout.write('Country {} - {} was registered'.format(
-                        country,
-                        abbreviation
-                    ))
-
-                else:
-                    try:
-                        state, abbreviation = self._parse_line(line)
-                    except ValueError:
-                        self.stderr.write('Probably the file format is wrong')
-
-                    State.objects.create(
-                        name=state,
-                        abbreviation=abbreviation,
-                        country_id=Country.objects.last().id
-                    )
-                    self.stdout.write('State {} - {} was registered'.format(
-                        state,
-                        abbreviation
-                    ))
-
-            self.stdout.write('\nAll regions were registered successfully!')
+            except yaml.parser.ParserError as pe:
+                self.stderr.write(str(pe))
+            except yaml.scanner.ScannerError as se:
+                self.stderr.write(str(se))
+            finally:
+                self.stdout.write('End seed')
