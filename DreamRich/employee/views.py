d994aebcb62939b374c19625df7acd5f710ff088
joaorobson <joaorobsonmartins@gmail.com>
2017-10-07 14:36:30 -0300

    Add employee permissions endpoint

--- DreamRich/employee/views.py
+++ DreamRich/employee/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from dr_auth.roles import *
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route, list_route
 from employee.serializers import (
     EmployeeSerializer,
     FinancialAdviserSerializer,
@@ -10,17 +13,22 @@ from employee.models import (
 from dr_auth.permissions import SomePermission, EmployeesPermission  
 
 class EmployeeViewSet(viewsets.ModelViewSet):
-    required_permission = 'see_client_basic_data'
+    required_permission = 'see_employee_data'
     permission_classes = (EmployeesPermission,)
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+
+    @list_route(methods=['get']) 
+    def employee_permissions(self, request):
+        queryset = Employee.objects.all()
+        employee_available_permissions = CommonEmployee().available_permissions 
+        return Response(employee_available_permissions)
     @detail_route
     def a(self, request, *args, **kwargs):
         print(request.user)
 
-
 class FinancialAdviserViewSet(viewsets.ModelViewSet):
-    required_permission = {'change_client_data'}
+    required_permission = 'see_financialadvisers_data'
 
     serializer_class = FinancialAdviserSerializer
     queryset = FinancialAdviser.objects.all()
