2531de01c9f452fcc6fd338a2057268bbc60e2d5
joaorobson <joaorobsonmartins@gmail.com>
2017-10-08 15:17:57 -0300

    Add financial advisers permissions

--- DreamRich/dr_auth/permissions.py
+++ DreamRich/dr_auth/permissions.py
@@ -2,11 +2,16 @@ from rest_framework import permissions
 import rolepermissions
 from rolepermissions import checkers
 
+class FinancialAdvisersPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if checkers.has_permission(request.user, 'see_financial_adviser_data'):
+            return True
+        return False 
+
 class EmployeesPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         current_user = view.queryset.filter(id=request.user.id).get()
-        print(request.user.id)
-        if not checkers.has_permission(request.user, 'see_financialadvisers_data'):
+        if checkers.has_permission(request.user, 'see_employee_data') and not checkers.has_permission(request.user, 'see_financial_adviser_data'):
             view.queryset = view.queryset.values().filter(id=request.user.id)
         return True
 
