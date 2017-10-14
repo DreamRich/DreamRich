09938401a7ad6a377b4f58e4274a9146f21b3fec
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-14 12:07:25 -0300

    Small fixes on autofix.py and stylesheet.py
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- src/dreamrich/management/commands/autofix.py
+++ src/dreamrich/management/commands/autofix.py
@@ -1,13 +1,14 @@
 # pylint: disable=unused-argument
 import os
 from django.core.management.base import BaseCommand
+from dreamrich.settings import BASE_DIR
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        self.stdout.write("Try apply autopep8 fixes")
-        path = os.path.realpath(".")
+        self.stdout.write("Try applying autopep8 fixes")
+        path = os.path.realpath(BASE_DIR)
         os.system(
-            "autopep8 --exclude=migrations,settings.py,__init__.py -iar %s" %
-            path)
+            "autopep8 --exclude=migrations,settings.py,__init__.py" +
+            "--inplace --aggressive --recursive '{}'".format(path))
--- src/dreamrich/management/commands/stylesheet.py
+++ src/dreamrich/management/commands/stylesheet.py
@@ -1,11 +1,12 @@
 # pylint: disable=unused-argument
 import os
 from django.core.management.base import BaseCommand
+from dreamrich.settings import BASE_DIR
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Executing stylesheet verification")
-        path = os.path.realpath(".")
-        os.system("pylint --load-plugins pylint_django {}".format(path))
+        path = os.path.realpath(BASE_DIR)
+        os.system("pylint --load-plugins pylint_django '{}'".format(path))
