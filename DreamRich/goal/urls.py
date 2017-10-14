3b807e960ddc0c632d469cffd9ce84f01be8bd6d
joaorobson <joaorobsonmartins@gmail.com>
2017-10-14 12:46:15 -0300

    Add goals urls
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: joaorobson <joaorobsonmartins@gmail.com>

--- DreamRich/goal/urls.py
+++ DreamRich/goal/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from goal.views import GoalManagerViewSet
+from goal.views import * 
 
 app_name = 'goal' # pylint: disable=invalid-name
 
 router = routers.DefaultRouter() # pylint: disable=invalid-name
-router.register(r'^dic', GoalManagerViewSet)
-
+router.register(r'^goal_manager', GoalManagerViewSet)
+router.register(r'^goal_type', GoalTypeViewSet)
+router.register(r'^goal', GoalViewSet)
 urlpatterns = router.urls
