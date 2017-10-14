70232efacbfc57dba14167604f657c65e8865fc5
Marcelo Ferreira <marcelohpf@gmail.com>
2017-10-14 12:26:01 -0300

    Create serializers to goal
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: João Robson <joaorobsonmartins@gmail.com>

--- DreamRich/goal/serializers.py
+++ DreamRich/goal/serializers.py
@@ -1,5 +1,32 @@
-from goal.models import GoalManager
+from goal.models import GoalManager, GoalType, Goal
 from rest_framework import serializers
+
+class GoalTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GoalType
+        fields = ['name', 'pk']
+
+class GoalSerializer(serializers.ModelSerializer):
+
+    goal_manager_id = serializers.IntegerField(write_only=True)
+    goal_type = GoalTypeSerializer(read_only=True)
+    goal_type_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Goal
+        fields = [
+            'id',
+            'value',
+            'periodicity',
+            'year_end',
+            'year_init',
+            'has_end_date',
+            'goal_type_id',
+            'goal_type',
+            'goal_manager_id'
+        ]
+
 
 
 class GoalManagerSerializer(serializers.ModelSerializer):
@@ -7,6 +34,7 @@ class GoalManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = GoalManager
         fields = [
+            'id',
             'goals_flow_dic',
             'year_init_to_year_end',
         ]
