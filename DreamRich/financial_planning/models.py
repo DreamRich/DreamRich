27c6b6eef7e512d7290b17cfd994395fb626398e
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-10-14 15:04:28 -0300

    Remove additional method to total_resource_for_annual_goals with real data

--- DreamRich/financial_planning/models.py
+++ DreamRich/financial_planning/models.py
@@ -166,8 +166,15 @@ class FinancialPlanning(models.Model):
             data[rate] = cdi_rate
         return data
 
-    def total_resource_for_annual_goals(self, annual_leftovers_for_goal,
-                                        total_goals, duration):
+    def total_resource_for_annual_goals(self, change_income={},
+                                             change_cost={}):
+
+        annual_leftovers_for_goal = self.annual_leftovers_for_goal(
+                                                change_income,
+                                                change_cost)
+        total_goals = self.goal_manager.value_total_by_year()
+        duration = self.duration()
+
         resource_for_goal = [0] * (duration)
         resource_for_goal[0] = annual_leftovers_for_goal[0]
         rate_dic = self.real_gain_related_cdi()
@@ -180,16 +187,3 @@ class FinancialPlanning(models.Model):
                                          resource_for_goal_monetized
 
         return resource_for_goal
-
-    def total_resource_for_annual_goals_real(self, change_income={},
-                                             change_cost={}):
-        # TODO Remover esse metodo e colocar esses atributos no metodo
-        # total_resource_for_annual_goals
-        annual_leftovers_for_goal = self.annual_leftovers_for_goal(
-                                                change_income,
-                                                change_cost)
-        total_goals = self.goal_manager.value_total_by_year()
-        duration = self.duration()
-        return self.total_resource_for_annual_goals(annual_leftovers_for_goal,
-                                                    total_goals,
-                                                    duration)
