3c1b878edf7eb6f6326eb4bfebe143a1cbdbf213
joaorobson <joaorobsonmartins@gmail.com>
2017-10-14 12:46:51 -0300

    Add goals views
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: joaorobson <joaorobsonmartins@gmail.com>

--- DreamRich/goal/views.py
+++ DreamRich/goal/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
-from goal.serializers import (
-    GoalManagerSerializer,
-)
+from goal.serializers import * 
+
 from goal.models import (
+    Goal,
+    GoalType,
     GoalManager,
 )
 
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    queryset = Goal.objects.all()
+
+class GoalTypeViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalTypeSerializer
+    queryset = GoalType.objects.all()
 
 class GoalManagerViewSet(viewsets.ModelViewSet):
     serializer_class = GoalManagerSerializer
