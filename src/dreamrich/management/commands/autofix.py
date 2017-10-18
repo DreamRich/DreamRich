1fca061189d378c421ab7d822f57c4d516b274ba
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-18 15:43:28 -0200

    Redo autofix script and move get_modules()to a separated file
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- src/dreamrich/management/commands/autofix.py
+++ src/dreamrich/management/commands/autofix.py
@@ -1,14 +1,25 @@
-# pylint: disable=unused-argument
 import os
+import subprocess
 from django.core.management.base import BaseCommand
 from dreamrich.settings import BASE_DIR
+from .utils.get_modules import get_modules_list
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        self.stdout.write("Try applying autopep8 fixes")
-        path = os.path.realpath(BASE_DIR)
-        os.system(
-            "autopep8 --exclude=migrations,settings.py,__init__.py" +
-            "--inplace --aggressive --recursive '{}'".format(path))
+        self.stdout.write("Executing autofix verification autopep8 fixes...")
+
+        src_folder = BASE_DIR + '/'
+        apps = get_modules_list()
+
+        self.stdout.write('The folowing module will be fixed: {}'
+                          .format(', '.join(apps)))
+
+        for app in apps:
+            self.stdout.write("Applying autofix on app {}...".format(app))
+            app = src_folder + app
+
+            subprocess.call(['autopep8', '--in-place', '--aggressive',
+                             '--aggressive', '--recursive', '--verbose',
+                             '--verbose', '--exclude', 'migrations', app])
--- src/dreamrich/management/commands/stylesheet.py
+++ src/dreamrich/management/commands/stylesheet.py
@@ -3,35 +3,10 @@ import os
 import subprocess
 from django.core.management.base import BaseCommand
 from dreamrich.settings import BASE_DIR
+from .utils.get_modules import get_modules_list
 
 
 class Command(BaseCommand):
-
-    def _get_modules_list(self):
-        inits_paths = subprocess.Popen(['find', '-name', '__init__.py'],
-                                       stdout=subprocess.PIPE)
-
-        modules = subprocess.Popen(['cut', '-d', '/', '-f', '2'],
-                                           stdin=inits_paths.stdout,
-                                           stdout=subprocess.PIPE)
-
-        modules, error = modules.communicate()
-
-        if error:
-            self.stderr.write("Wasn't possible to run bash" +
-                              " commands, probably your environment" +
-                              " is not compatible or you don't" + 
-                              " have permissions")
-            exit(1)
-
-        else:
-            modules = modules.decode('utf-8')
-            modules = set(modules.split('\n'))
-
-        modules = list(modules)
-        modules = [module for module in modules if len(module)]
-
-        return modules
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Executing stylesheet verification")
@@ -39,13 +14,13 @@ class Command(BaseCommand):
         src_folder = BASE_DIR + '/'
         pylintrc_file = src_folder + '../.pylintrc'
 
-        apps = self._get_modules_list()
+        apps = get_modules_list()
 
         self.stdout.write('The folowing modules will be verified: {}'
                           .format(', '.join(apps)))
 
         for app in apps:
             self.stdout.write('Scanning app {}...'.format(app))
-
             app = src_folder + app
+
             subprocess.call(['pylint', '--rcfile', pylintrc_file, app])
