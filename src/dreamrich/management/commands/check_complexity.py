b69c0ef58cb437aa594ffa83e6534a2aed8f4676
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-20 16:34:30 -0200

    Refactor code_complexity and rename it, in addition to others
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

---
+++ src/dreamrich/management/commands/check_complexity.py
@@ -0,0 +1,27 @@
+# pylint: disable=unused-argument
+import subprocess
+from django.core.management.base import BaseCommand
+from .utils.general import get_modules, apply_to_all_modules, get_script_name
+
+EXCLUDE_COMMAND = ['--exlude manage.py, __init__.py, migrations']
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def _call_radon_raw(module):
+        returncode = subprocess.call(['radon', 'raw', module] + EXCLUDE_COMMAND)
+        return returncode
+
+    @staticmethod
+    def _call_radon_cc(module):
+        returncode = subprocess.call(['radon', 'cc', module] + EXCLUDE_COMMAND)
+        return returncode
+
+    def handle(self, *args, **kwargs):
+        script_name = get_script_name(__file__)
+
+        self.stdout.write("Analysing raw metrics...\n")
+        apply_to_all_modules(self._call_radon_raw, script_name)
+
+        self.stdout.write("Analysing cyclomatic-complexity")
+        apply_to_all_modules(self._call_radon_cc, script_name)
