d4c4ecd390b853bd4b165c1b7a73efaf6da7df28
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-10-24 12:27:36 -0200

    Test save of financial planning with init_year

--- DreamRich/financial_planning/test.py
+++ DreamRich/financial_planning/test.py
@@ -157,6 +157,11 @@ class FinancialPlanningTest(TestCase):
         self.assertEqual(self.financial_planning.
                          total_resource_for_annual_goals, array)
 
+    def test_save_financial_planning(self):
+        financial_planning = FinancialPlanningFactory(init_year=None)
+        self.assertEqual(financial_planning.init_year,
+                         datetime.datetime.now().year)
+
 
 class RegularCostViewTest(TestCase):
 
