773d90babd665622cffc2e117a90763e6b815d4e
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-24 22:49:29 -0200

    Make recent scripts run from everywhere
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- src/dreamrich/management/commands/utils/general.py
+++ src/dreamrich/management/commands/utils/general.py
@@ -3,25 +3,29 @@ import sys
 import subprocess
 from dreamrich.settings import BASE_DIR
 
+SRC_FOLDER = os.path.join(BASE_DIR, 'src')
 
 def get_modules():
-    inits_paths = subprocess.Popen(['find', 'src', '-name', '__init__.py'],
+    get_abs_paths = subprocess.Popen(['find', SRC_FOLDER, '-name', '__init__.py'],
                                    stdout=subprocess.PIPE)
+    find_error = get_abs_paths.wait()
 
-    modules = subprocess.Popen(['cut', '-d', '/', '-f', '2'],
-                               stdin=inits_paths.stdout,
-                               stdout=subprocess.PIPE)
+    remove_leading = subprocess.Popen(['sed', 's@{}@@'.format(SRC_FOLDER)],
+                                      stdin=get_abs_paths.stdout,
+                                      stdout=subprocess.PIPE)
+    sed_error = remove_leading.wait()
 
-    modules, error = modules.communicate()
+    remove_trailing = subprocess.Popen(['cut', '-d', '/', '-f', '2'],
+                                      stdin=remove_leading.stdout,
+                                      stdout=subprocess.PIPE)
+    cut_error = remove_leading.wait()
 
-    if error:
-        print("Wasn't possible to run bash" +
-              " commands, probably your environment" +
-              " is not compatible or you don't" +
-              " have permissions", file=sys.stderr)
+    modules, _ = remove_trailing.communicate()
 
+    if find_error or sed_error or cut_error:
+        print("Some bash command returned an error, is not possible to get" +
+              " the modules list", file=sys.stderr)
         exit(1)
-
     else:
         modules = modules.decode('utf-8')
         modules = set(modules.split('\n'))
@@ -52,9 +56,10 @@ def apply_to_all_modules(function, script_name):
     print('{} will be applied on the following modules: {}'
           .format(script_name, ', '.join(modules)))
 
+    has_error = False
     for module in modules:
         print('\nApplying {} to module {}...'.format(script_name, module))
-        module = os.path.join(BASE_DIR, 'src', module)
+        module = os.path.join(SRC_FOLDER, module)
 
         returncode = function(module)
         has_error = True if returncode else False
