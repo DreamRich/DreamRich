e5203df8153b9f23ec1868a27755832b10efd2aa
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-26 01:37:42 -0200

    Refactor test scripts
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- src/dreamrich/management/commands/test_coverage.py
+++ src/dreamrich/management/commands/test_coverage.py
@@ -1,18 +1,24 @@
-# pylint: disable=unused-argument
 import os
+import subprocess
+from dreamrich.settings import BASE_DIR
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Erase last coverage analysis")
-        os.system("coverage erase")
-        self.stdout.write("Executing tests")
-        os.system("coverage run manage.py test")
-        self.stdout.write("Generating html report")
-        os.system("coverage html")
-        path = os.path.realpath(".")
-        message = "Open the link in your browser\n\tfile:///{}/\
-                   htmlcov/index.html".format(path)
+    def handle(self, *args, **unused_kwargs):
+        manage_path = os.path.join(BASE_DIR, 'manage.py')
+
+        self.stdout.write("Erasing last coverage analysis...")
+        subprocess.call(['coverage', 'erase'])
+
+        self.stdout.write("Executing tests...")
+        subprocess.call(['coverage', 'run', manage_path, 'test'])
+
+        self.stdout.write("Generating html report...")
+        subprocess.call(['coverage', 'html'])
+
+        htmlcov_path = os.path.realpath(".")
+        message = ("Open the following link in your browser:\n"
+                   "\tfile:///{}/htmlcov/index.html").format(htmlcov_path)
         self.stdout.write(message)
--- src/dreamrich/management/commands/test_report.py
+++ src/dreamrich/management/commands/test_report.py
@@ -1,12 +1,16 @@
-# pylint: disable=unused-argument
+import subprocess
 import os
+from dreamrich.settings import BASE_DIR
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Executing tests")
-        os.system("coverage run manage.py test")
-        self.stdout.write("Loading results")
-        os.system("coverage report --show-missing")
+    def handle(self, *args, **unused_kwargs):
+        manage_path = os.path.join(BASE_DIR, 'manage.py')
+
+        self.stdout.write("Executing tests...")
+        subprocess.call(['coverage', 'run', manage_path, 'test'])
+
+        self.stdout.write("Loading results...")
+        subprocess.call(['coverage', 'report', '--show-missing'])
