3940d8f51beb2ee835290a2ea714278dc2cb7707
joaorobson <joaorobsonmartins@gmail.com>
2017-11-02 14:58:05 -0200

    Add endpoint to get each client's financial planning and goal manager

--- src/financial_planning/views.py
+++ src/financial_planning/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from financial_planning.serializers import (
     RegularCostSerializer,
     CostManagerSerializer,
@@ -32,3 +34,26 @@ class FinancialPlanningViewSet(viewsets.ModelViewSet):
 
     serializer_class = FinancialPlanningSerializer
     queryset = FinancialPlanning.objects.all()
+
+    @detail_route(methods=['get'])
+    def respective_clients(self, request, pk=None):
+        current_FinancialAdviser = FinancialAdviser.objects.filter(
+            id=request.user.id).get()
+        clients_ids_list = []
+        # fp = Financial Planning and gm = Goal Manager
+        fp_and_gm_pks = {}
+        clients_ids = current_FinancialAdviser.clients.all().values('id')
+        for i in clients_ids:
+            clients_ids_list.append(list(i.values())[0])
+
+        if int(pk) in clients_ids_list:
+            try:
+                financial_planning_pk = current_FinancialAdviser.clients.filter(
+                    id=pk).get().financialplanning.pk
+                goal_manager_pk = current_FinancialAdviser.clients.filter(
+                    id=pk).get().financialplanning.goal_manager.pk
+                fp_and_gm_pks = (
+                    {'fp': financial_planning_pk, 'gm': goal_manager_pk})
+            except BaseException:
+                fp_and_gm_pks = ({'fp': '', 'gm': ''})
+        return Response(fp_and_gm_pks)
