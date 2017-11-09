99946abc9b49fd26f2845bea5af24ed4d764c43d
joaorobson <joaorobsonmartins@gmail.com>
2017-11-08 23:24:05 -0200

    Refactor method to get goal manager and financial planning pks

--- src/financial_planning/views.py
+++ src/financial_planning/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import detail_route
 from financial_planning.serializers import (
     RegularCostSerializer,
@@ -14,7 +15,7 @@ from financial_planning.models import (
     FinancialPlanning,
 )
 from employee.models import FinancialAdviser
-
+from dr_auth.permissions import FinancialPlanningPermission
 
 class CostTypeViewList(generics.ListAPIView):
     serializer_class = CostTypeSerializer
@@ -32,31 +33,32 @@ class CostManagerViewSet(viewsets.ModelViewSet):
 
 
 class FinancialPlanningViewSet(viewsets.ModelViewSet):
-
     serializer_class = FinancialPlanningSerializer
     queryset = FinancialPlanning.objects.all()
 
     @detail_route(methods=['get'])
+    def total_resource_for_annual_goals(self, request, pk=None):
+        return Response(FinancialPlanning.objects.filter(pk=pk).get().total_resource_for_annual_goals)
+
+
+    @detail_route(methods=['get'])
     # pylint: disable=invalid-name, no-self-use
     def respective_clients(self, request, pk=None):
-        current_financial_adviser = FinancialAdviser.objects.filter(
-            id=request.user.id).get()
-        clients_ids_list = []
-        # fp = Financial Planning and gm = Goal Manager
-        fp_and_gm_pks = {}
-        clients_ids = current_financial_adviser.clients.all().values('id')
-        for ids in clients_ids:
-            clients_ids_list.append(list(ids.values())[0])
+        if FinancialPlanningPermission.has_permission_to_see_chart(request):
+            current_financial_adviser = FinancialAdviser.objects.filter(
+                id=request.user.id).get()
 
-        if int(pk) in clients_ids_list:
-            try:
-                financial_adviser_clients = current_financial_adviser.clients
-                financial_planning_pk = financial_adviser_clients.filter(
-                    id=pk).get().financialplanning.pk
-                goal_manager_pk = current_financial_adviser.clients.filter(
-                    id=pk).get().financialplanning.goal_manager.pk
-                fp_and_gm_pks = (
-                    {'fp': financial_planning_pk, 'gm': goal_manager_pk})
-            except BaseException:
-                fp_and_gm_pks = ({'fp': '', 'gm': ''})
-        return Response(fp_and_gm_pks)
+            # fp = Financial Planning and gm = Goal Manager
+            fp_and_gm_pks = ()
+
+            if current_financial_adviser.clients.filter(pk=pk).exists():
+                try:
+                    goal_manager_pk = current_financial_adviser.clients.filter(
+                        id=pk).get().financialplanning.goal_manager.pk
+                    fp_and_gm_pks = (
+                        {'fp': pk, 'gm': goal_manager_pk})
+                except BaseException:
+                    fp_and_gm_pks = ({'fp': '', 'gm': ''})
+            return Response(fp_and_gm_pks)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
