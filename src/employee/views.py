f874bb508242f315635697b9b585208692a59061
Sabryna de Sousa <sabryna.sousa1323@gmail.com>
2017-11-24 08:34:01 -0200

    Create doc api of employee

--- src/employee/views.py
+++ src/employee/views.py
@@ -14,6 +14,26 @@ from dr_auth.permissions import (
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return a employee instance.
+
+    list:
+        Return all employees, ordered by most recently joined.
+
+    create:
+        Create a new employee.
+
+    delete:
+        Remove an existing employee.
+
+    partial_update:
+        Update one or more fields on an existing employee.
+
+    update:
+        Update a employee.
+    """
+
     required_permission = 'see_employee_data'
     permission_classes = (EmployeesPermission,)
     serializer_class = EmployeeSerializer
@@ -21,6 +41,26 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
 
 class FinancialAdviserViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return a financial adviser.
+
+    list:
+        Return all financial advisers, ordered by most recently joined.
+
+    create:
+        Create a new financial adviser.
+
+    delete:
+        Remove an existing financial adviser.
+
+    partial_update:
+        Update one or more fields on an existing financial adviser.
+
+    update:
+        Update a financial adviser.
+    """
+
     required_permission = 'see_employee_data'
 
     serializer_class = FinancialAdviserSerializer
