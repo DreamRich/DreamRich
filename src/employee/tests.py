c0b830aa684bd822c4917068445cfd841ae56d4b
Ícaro Pires <icaropiresdf@gmail.com>
2017-12-04 18:42:14 -0200

    Add base tests for Financial Adviser permissions
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- src/employee/tests.py
+++ src/employee/tests.py
@@ -137,3 +137,140 @@ class EmployeePermissionsTest(TestCase):
         )
 
         self.assertEqual(response.status_code, 403)
+
+
+class FinancialAdviserPermissionsTest(TestCase):
+
+    def setUp(self):
+        self.django_client = APIClient()
+        self.financial_adviser = FinancialAdviserMainFactory()
+        self.other_financial_adviser = FinancialAdviserMainFactory()
+
+        # Authenticate user
+        self.token = 'JWT {}'.format(get_token(self.financial_adviser))
+        self.django_client.credentials(HTTP_AUTHORIZATION=self.token)
+
+    @staticmethod
+    def _get_route(element=0):
+        route = '/api/employee/financial/'
+
+        if element:
+            route += '{}/'.format(element)
+        else:
+            route = route
+
+        return route
+
+    def test_financial_adviser_own_data_get(self):
+        route = self._get_route(self.financial_adviser.pk)
+        response = self.django_client.get(route)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_financial_adviser_own_data_put(self):
+        route = self._get_route(self.financial_adviser.pk)
+
+        response = self.django_client.put(
+            route,
+            json.dumps({
+                'first_name': 'Jeremy',
+                'last_name': 'Dixon',
+                'cpf': '68205793492',
+                'email': ''
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_financial_adviser_own_data_patch(self):
+        route = self._get_route(self.financial_adviser.pk)
+
+        response = self.django_client.patch(
+            route,
+            json.dumps({
+                'last_name': 'Lee',
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_financial_adviser_own_data_delete(self):
+        route = self._get_route(self.financial_adviser.pk)
+        response = self.django_client.delete(route)
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_get_other_financial_adviser_data(self):
+        other_financial_adviser_pk = self.other_financial_adviser.pk
+
+        route = self._get_route(other_financial_adviser_pk)
+        response = self.django_client.get(route)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_put_other_financial_adviser_data(self):
+        other_financial_adviser_pk = self.other_financial_adviser.pk
+
+        route = self._get_route(other_financial_adviser_pk)
+
+        response = self.django_client.put(
+            route,
+            json.dumps({
+                'pk': other_financial_adviser_pk,
+                'first_name': 'Maria',
+                'last_name': 'Old',
+                'cpf': '72986145094',
+                'email': ''
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_patch_other_financial_adviser_data(self):
+        other_financial_adviser_pk = self.other_financial_adviser.pk
+
+        route = self._get_route(other_financial_adviser_pk)
+
+        response = self.django_client.patch(
+            route,
+            json.dumps({
+                'last_name': 'Old',
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_other_financial_adviser_data(self):
+        other_financial_adviser_pk = self.other_financial_adviser.pk
+
+        route = self._get_route(other_financial_adviser_pk)
+        response = self.django_client.delete(route)
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_financial_adviser_get_list(self):
+        route = self._get_route()
+
+        response = self.django_client.get(route)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_financial_adviser_post(self):
+        route = self._get_route()
+
+        response = self.django_client.post(
+            route,
+            json.dumps({
+                'first_name': 'Jeremy',
+                'last_name': 'Dixon',
+                'cpf': '68205793492',
+                'email': ''
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 201)
