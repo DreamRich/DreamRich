b4d1ffd4faab1ffca205c794bee4fdd1c6754be1
Vinicius Ferreira <vfbdelima@gmail.com>
2017-12-12 15:47:34 -0200

    Add commom financial to arrearage
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- src/patrimony/models.py
+++ src/patrimony/models.py
@@ -187,10 +187,10 @@ class ArrearageCalculator:
                 self.calculate_amortization(period)
             parameter_list = {
                 'period': period,
-                'provision': self.calculate_provision(period),
-                'interest': self.calculate_interest(period),
-                'amortization': self.calculate_amortization(period),
-                'outstanding_balance': outstanding_balance
+                'provision': round(self.calculate_provision(period), 2),
+                'interest': round(self.calculate_interest(period), 2),
+                'amortization': round(self.calculate_amortization(period), 2),
+                'outstanding_balance': round(outstanding_balance, 2)
             }
             data.append(parameter_list)
 
@@ -209,10 +209,12 @@ class ArrearageCalculator:
                     (period - 1) * value / calculated_period
                 )
             ) * (rate / 100))
-        else:
+        elif self.calculate.amortization_system == AMORTIZATION_CHOICES[1][0]:
             provision = self.calculate_provision(period)
             amortization = self.calculate_amortization(period)
             interest = provision - amortization
+        else:
+            interest = 0
 
         return interest
 
@@ -220,13 +222,16 @@ class ArrearageCalculator:
         amortization = 0
         if self.calculate.amortization_system == AMORTIZATION_CHOICES[0][0]:
             amortization = self.calculate.value / self.calculate.period
-        else:
+        elif self.calculate.amortization_system == AMORTIZATION_CHOICES[1][0]:
             first_amortization = (
                 self.calculate_provision(1) -
                 (self.calculate.value * (self.calculate.rate / 100))
             )
             amortization = first_amortization * \
                 ((1 + (self.calculate.rate / 100))**(period - 1))
+        else:
+            amortization = self.calculate.value / self.calculate.period
+
         return amortization
 
     def calculate_provision(self, period):
@@ -240,11 +245,13 @@ class ArrearageCalculator:
             amortization = self.calculate_amortization(period)
             interest = self.calculate_interest(period)
             provision = amortization + interest
-        else:
+        elif self.calculate.amortization_system == AMORTIZATION_CHOICES[1][0]:
             provision = (
                 value * (rate / 100) /
                 (1 - (1 + (rate / 100))**(-calculated_period))
             )
+        else:
+            provision = self.calculate.value / self.calculate.period
 
         return provision
 
@@ -253,9 +260,8 @@ class Arrearage(models.Model):
     name = models.CharField(max_length=100)
     value = models.FloatField(default=0)
     actual_period = models.PositiveIntegerField(default=0)
-    period = models.PositiveIntegerField(default=0)
+    period = models.PositiveIntegerField(default=1)
     rate = models.FloatField(
-        default=0,
         validators=[
             MinValueValidator(
                 patrimony_validators.MIN_VALUE_RATE,
@@ -270,7 +276,6 @@ class Arrearage(models.Model):
     amortization_system = models.CharField(
         max_length=5,
         choices=AMORTIZATION_CHOICES,
-        default=AMORTIZATION_CHOICES[0][0]
     )
     patrimony = models.ForeignKey(
         Patrimony, on_delete=models.CASCADE,
