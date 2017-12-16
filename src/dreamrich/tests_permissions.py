6b80d58fe0d4f9246083234e74d309c8cb060d0c
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2017-12-16 17:11:37 -0200

    Componentize make_request for permissions tests
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- src/dreamrich/tests_permissions.py
+++ src/dreamrich/tests_permissions.py
@@ -48,44 +48,44 @@ class PermissionsTests(TestCase):
         self.assertEqual(response_status_code, status_code)
 
     def make_request(self, test_type, route=None):
-        require_data_methods = [RequestTypes.PUT,
-                                RequestTypes.PATCH,
-                                RequestTypes.POST]
-
-        # Validate params
-        if test_type not in RequestTypes:
-            raise AttributeError('Invalid http method')
-
-        # Handle test_types that are not http_methods
-        if test_type == RequestTypes.GETLIST:
-            http_method = RequestTypes.GET.value
-        else:
-            http_method = test_type.value
-
-        method = getattr(self.django_client, http_method)
         route = self._get_route(test_type) if not route else route
 
+        http_method = self._handle_test_type(test_type)
+        method = getattr(self.django_client, http_method)
+
+        required_data_methods = [RequestTypes.PUT,
+                                 RequestTypes.PATCH,
+                                 RequestTypes.POST]
+
         # Prepare data and make request
-        if test_type in require_data_methods:
-            data = self.serializer_consulted(  # pylint: disable=not-callable
-                self.consulted_instance
+        if test_type in required_data_methods:
+            response = self._make_required_data_request(
+                test_type,
+                method,
+                route
             )
-            data = data.data
-
-            if test_type == RequestTypes.PATCH:  # Send data just to one field
-                field = data.popitem()
-                data = {field[0]: field[1]}
-            elif test_type == RequestTypes.POST:  # Make a new one
-                data.pop('id')
-                data['cpf'] = '75116625109'  # Generated online
-
-            data = json.dumps(data)
-
-            response = method(route, data, content_type='application/json')
         else:
             response = method(route)
 
         return response.status_code
+
+    def _make_required_data_request(self, test_type, method, route):
+        data = self.serializer_consulted(  # pylint: disable=not-callable
+            self.consulted_instance
+        )
+        data = data.data
+
+        if test_type == RequestTypes.PATCH:  # Send data just to one field
+            field = data.popitem()
+            data = {field[0]: field[1]}
+        elif test_type == RequestTypes.POST:  # Make a new one
+            data.pop('id')
+            data['cpf'] = '75116625109'  # Generated online
+
+        data = json.dumps(data)
+        response = method(route, data, content_type='application/json')
+
+        return response
 
     def _get_route(self, test_type):
         general_routes_tests = [RequestTypes.POST, RequestTypes.GETLIST]
@@ -96,6 +96,19 @@ class PermissionsTests(TestCase):
             route = self.base_route
 
         return route
+
+    @staticmethod
+    def _handle_test_type(test_type):
+        if test_type not in RequestTypes:
+            raise AttributeError('Invalid http method')
+
+        # Handle test_types that are not http_methods
+        if test_type == RequestTypes.GETLIST:
+            http_method = RequestTypes.GET.value
+        else:
+            http_method = test_type.value
+
+        return http_method
 
 
 class UserToClient(PermissionsTests):
