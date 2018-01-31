0c15fbef11be862732f6719245008db87f6d2079
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2018-01-31 19:09:52 -0200

    Add base class for custom permissions
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- src/dr_auth/permissions.py
+++ src/dr_auth/permissions.py
@@ -2,39 +2,121 @@ from rest_framework.permissions import BasePermission
 from dreamrich.utils import Relationship
 
 
-class ClientsPermissions(BasePermission):
+class BaseCustomPermissions(BasePermission):
+
+    class Meta:
+        abstract = True
+
+    authorized = False
+    view = None
+    request = None
+    user = None
+
+    _not_implemented_error_message = ('This method must be implemented at all'
+                                      ' children classes')
 
     def has_permission(self, request, view):
-        user_pk = str(request.user.pk)
+        self.view = view
+        self.request = request
+        self.user = self.request.user
 
-        if (request.user.has_perm('client.see_own_client') and
-                view.action == 'retrieve' and
-                view.kwargs['pk'] == user_pk):
-            authorized = True
-        elif (request.user.has_perm('client.change_own_client') and
-                (view.action == 'update' or
-                 view.action == 'partial_update') and
-                view.kwargs['pk'] == user_pk):
-            authorized = True
-        else:
-            authorized = True
+        authorized = False
+        if view.action == 'list':
+            authorized = self.list()
+        elif view.action == 'retrieve':
+            authorized = self.retrieve()
+        elif view.action == 'create':
+            authorized = self.create()
+        elif view.action in ('update', 'partial_update'):
+            authorized = self.update()
+        elif view.action == 'destroy':
+            authorized = self.destroy()
 
         return authorized
 
+    def list(self):
+        raise NotImplementedError(self._not_implemented_error_message)
 
-class EmployeesPermissions(BasePermission):
+    def retrieve(self):
+        raise NotImplementedError(self._not_implemented_error_message)
 
-    def has_permission(self, request, view):
-        return True
+    def create(self):
+        raise NotImplementedError(self._not_implemented_error_message)
+
+    def update(self):
+        raise NotImplementedError(self._not_implemented_error_message)
+
+    def destroy(self):
+        raise NotImplementedError(self._not_implemented_error_message)
 
 
-class FinancialAdvisersPermissions(BasePermission):
+class ClientsPermissions(BaseCustomPermissions):
 
-    def has_permission(self, request, view):
-        return True
+    def list(self):
+        return False
+
+    def retrieve(self):
+        return False
+
+    def create(self):
+        return False
+
+    def update(self):
+        return False
+
+    def destroy(self):
+        return False
 
 
-class GeneralPermissions(BasePermission):
+class EmployeesPermissions(BaseCustomPermissions):
 
-    def has_permission(self, request, view):
-        return True
+    def list(self):
+        return False
+
+    def retrieve(self):
+        return False
+
+    def create(self):
+        return False
+
+    def update(self):
+        return False
+
+    def destroy(self):
+        return False
+
+
+class FinancialAdvisersPermissions(BaseCustomPermissions):
+
+    def list(self):
+        return False
+
+    def retrieve(self):
+        return False
+
+    def create(self):
+        return False
+
+    def update(self):
+        return False
+
+    def destroy(self):
+        return False
+
+
+class GeneralPermissions(BaseCustomPermissions):
+
+    def list(self):
+        return False
+
+    def retrieve(self):
+        return False
+
+    def create(self):
+        return False
+
+    def update(self):
+        return False
+
+    def destroy(self):
+        return False
