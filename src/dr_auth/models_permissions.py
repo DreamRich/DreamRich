93a0b943bb80be3ecde2b398f05728301906c83f
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2018-02-10 15:39:05 -0200

    Fix name for lists and change actions for permissions
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- src/dr_auth/models_permissions.py
+++ src/dr_auth/models_permissions.py
@@ -4,22 +4,22 @@ CLIENT_DEFAULT_CODENAMES_PERMISSIONS = [
 ]
 
 EMPLOYEE_DEFAULT_CODENAMES_PERMISSIONS = [
-    'retrieve_all_clients', 'add_clients', 'list_clients',
-    'retrieve_related_employees', 'update_related_employees'
+    'retrieve_all_clients', 'create_clients', 'list_all_clients',
+    'retrieve_related_employees', 'update_related_employees',
 ]
 
 # fa = financial adviser
 FINANCIAL_ADVISER_DEFAULT_CODENAMES_PERMISSIONS = [
     'retrieve_all_clients', 'destroy_related_clients', 'update_related_clients',
-    'list_clients',
+    'list_all_clients', 'create_clients',
 
-    'retrieve_all_employees', 'add_employees', 'destroy_all_employees',
-    'list_employees',
+    'retrieve_all_employees', 'create_employees', 'destroy_all_employees',
+    'list_all_employees',
 
-    'retrieve_all_fa', 'add_employees', 'destroy_all_fa', 'update_all_fa',
-    'retrieve_fa_list',
+    'retrieve_all_fa', 'create_fa', 'destroy_all_fa', 'update_all_fa',
+    'list_all_fa',
 
-    'retrieve_all_general', 'list_general', 'add_generals',
+    'retrieve_all_general', 'list_all_general', 'create_general',
     'destroy_related_general', 'update_related_general',
 ]
 
@@ -27,9 +27,9 @@ CLIENT_MODEL_PERMISSIONS = (
     ('retrieve_all_clients', 'retrieve all clients from database'),
     ('retrieve_related_clients', 'retrieve related clients (or itself, if clients)'),
     ('retrieve_clients_from_others', 'retrieve clients which others related'),
-    ('list_clients', 'retrieve list of clients itself'),
+    ('list_all_clients', 'retrieve list of clients itself'),
 
-    ('add_clients', 'Create a clients'),
+    ('create_clients', 'Create a clients'),
 
     ('update_all_clients', 'update all clients from database'),
     ('update_related_clients', 'update related clients (itself, if client)'),
@@ -44,9 +44,9 @@ EMPLOYEE_MODEL_PERMISSIONS = (
     ('retrieve_all_employees', 'retrieve all employees from database'),
     ('retrieve_related_employees', 'retrieve related employeess (itself, if employees)'),
     ('retrieve_employees_from_others', 'retrieve employeess which others related'),
-    ('list_employees', 'retrieve list of employeess itself'),
+    ('list_all_employees', 'retrieve list of employeess itself'),
 
-    ('add_employees', 'Create an employees'),
+    ('create_employees', 'Create an employees'),
 
     ('update_all_employees', 'update all employees from database'),
     ('update_related_employees', 'update related employeess (or itself)'),
@@ -60,9 +60,9 @@ FINANCIAL_ADVISER_MODEL_PERMISSIONS = (
     ('retrieve_all_fa', 'retrieve all fa, relateds and others'),
     ('retrieve_related_fa', 'retrieve related fa (or itself, if fa)'),
     ('retrieve_fa_from_others', 'retrieve fa which others related'),
-    ('retrieve_fa_list', 'retrieve list of fa itself'),
+    ('list_all_fa', 'retrieve list of fa itself'),
 
-    ('add_fa', 'Create an fa'),
+    ('create_fa', 'Create an fa'),
 
     ('update_all_fa', 'update all fa from database'),
     ('update_related_fa', 'update related fa (itself, if fa)'),
@@ -77,9 +77,9 @@ GENERAL_MODEL_PERMISSIONS = (
     ('retrieve_all_general', 'retrieve all general from database'),
     ('retrieve_related_general', 'retrieve related generals)'),
     ('retrieve_general_from_others', 'retrieve others generals)'),
-    ('list_general', 'retrieve list of generals itself'),
+    ('list_all_general', 'retrieve list of generals itself'),
 
-    ('add_generals', 'Create a general'),
+    ('create_general', 'Create a general'),
 
     ('update_all_general', 'update all general from database'),
     ('update_related_general', 'update related generals (itself, if general)'),
