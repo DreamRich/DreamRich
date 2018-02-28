d4c68129384fdf301217c803b6f9076d6cacb057
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2018-02-28 18:58:39 -0300

    Remove NotAuthenticatedToItselfTests and add AuthenticatedTests which will generate its tests
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- src/dr_auth/common_tests_permissions.py
+++ src/dr_auth/common_tests_permissions.py
@@ -1,3 +1,31 @@
+class MetaTestsGenerator(type):
+
+    def __init__(cls, name, bases, unused_dict):
+        if len(cls.mro()) > 2:  # Start on children classes
+            cls.generate_tests()
+
+        super(MetaTestsGenerator, cls).__init__(name, bases, dict)
+
+
+class AuthenticatedTests(metaclass=MetaTestsGenerator):
+
+    @classmethod
+    def generate_tests(cls):
+        if cls.expected_status_codes:
+            for action, status_code in cls.expected_status_codes:
+                def test_action(self):
+                    # pylint: disable=cell-var-from-loop
+                    self.user_test_request(action, status_code)
+
+                test_name = 'test_' + action
+                setattr(AuthenticatedTests, test_name, test_action)
+        else:
+            raise AttributeError(
+                "For using 'AuthenticatedTests', the class must"
+                " set a 'expected_status_codes' attribute."
+            )
+
+
 class NotAuthenticatedTests:
     # pylint: disable=not-callable
 
@@ -11,24 +39,6 @@ class NotAuthenticatedTests:
 
     def test_user_create_not_authenticated(self):
         self.user_test_not_authenticated_request('create')
-
-    def test_user_delete_not_authenticated(self):
-        self.user_test_not_authenticated_request('destroy')
-
-    def test_user_update_not_authenticated(self):
-        self.user_test_not_authenticated_request('update')
-
-    def test_user_partial_update_not_authenticated(self):
-        self.user_test_not_authenticated_request('partial_update')
-
-
-class NotAuthenticatedToItselfTests:
-    # pylint: disable=not-callable
-
-    user_test_not_authenticated_request = None  # pylint: disable=invalid-name
-
-    def test_user_retrieve_not_authenticated(self):
-        self.user_test_not_authenticated_request('retrieve')
 
     def test_user_delete_not_authenticated(self):
         self.user_test_not_authenticated_request('destroy')
